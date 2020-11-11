ca85cc417b70cc166058251a49e0b1bc710a8770
GDIBass <matj1985@gmail.com>
2020-11-10 21:32:04 -0800

    Second challenge done

---
+++ src/academy/learnprogramming/SpeedConverter.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming;
+
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        long miles = toMilesPerHour(kilometersPerHour);
+
+        String output = miles == -1
+                ? "Invalid Value"
+                : kilometersPerHour + " km/h = " + miles + " mi/h";
+        System.out.println(output);
+    }
+}
