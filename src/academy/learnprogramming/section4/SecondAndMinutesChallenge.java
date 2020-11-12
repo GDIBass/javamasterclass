863556a71a09096f7e550b39fc0ffe15becfa9d4
GDIBass <matj1985@gmail.com>
2020-11-11 21:27:44 -0800

    SecondAndMinutesChallenge done

--- src/academy/learnprogramming/section4/SecondAndMinutesChallenge.java
+++ src/academy/learnprogramming/section4/SecondAndMinutesChallenge.java
@@ -1,24 +1,34 @@
 package academy.learnprogramming.section4;
 
 public class SecondAndMinutesChallenge {
+
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
+
     public static void main(String[] args) {
         System.out.println(getDurationString(65L, 45L));
-        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(3945L));
+        System.out.println(getDurationString(-41));
+        System.out.println(getDurationString(3909L));
     }
 
     private static String getDurationString(long minutes, long seconds) {
         if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        String hoursString = (hours < 10 ? "0" : "") + hours + "h";
+        String minutesString = (remainingMinutes < 10 ? "0" : "") + remainingMinutes + "m";
+        String secondsString = (seconds < 10 ? "0" : "") + seconds + "s";
+
+        return hoursString + " " + minutesString + " " + secondsString;
     }
 
     private static String getDurationString(long seconds) {
         if (seconds < 0) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         long minutes = seconds / 60;
