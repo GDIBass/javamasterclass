2e870247362b73ae8fbe06d28313b5546bbf0b7a
GDIBass <matj1985@gmail.com>
2020-11-19 23:49:12 -0800

    LinkedList2

--- src/academy/learnprogramming/section8/Main.java
+++ src/academy/learnprogramming/section8/Main.java
@@ -1,9 +1,7 @@
 package academy.learnprogramming.section8;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList2.main(args);
+        academy.learnprogramming.section8.linkedlist.Demo.main(args);
     }
 }
---
+++ src/academy/learnprogramming/section8/linkedlist/Demo.java
@@ -0,0 +1,66 @@
+package academy.learnprogramming.section8.linkedlist;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+        while (stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);
+            if (comparison == 0) {
+                // equal, do not add
+                System.out.println(newCity + " is already included as a destination");
+                return false;
+            } else if (comparison > 0) {
+                // new city is before this one
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            }
+            // else move on
+        }
+        stringListIterator.add(newCity);
+        return true;
+    }
+
+    public static void mainOld(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<>();
+        placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Brisbane");
+        placesToVisit.add("Perth");
+        placesToVisit.add("Canberra");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Darwin");
+        printList(placesToVisit);
+
+        placesToVisit.add(1, "Alice Springs");
+        printList(placesToVisit);
+
+        placesToVisit.remove(4);
+        printList(placesToVisit);
+    }
+
+    private static void printList(LinkedList<String> linkedList) {
+        for (String s : linkedList) {
+            System.out.println("Now visiting: " + s);
+        }
+        System.out.println("=================");
+    }
+
+    private static void printListOld(LinkedList<String> linkedList) {
+        Iterator<String> i = linkedList.iterator();
+        while (i.hasNext()) {
+            System.out.println("Now visiting: " + i.next());
+        }
+        System.out.println("=================");
+    }
+
+
+}
